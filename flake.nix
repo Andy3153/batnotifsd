a33e0fa8e3bac1d060d9e3d0ef3eb796414edbcb
Andy3153 <andy3153@protonmail.com>
2024-09-11 19:25:58 +0300

    flake: minor tweaks

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 # vim: set fenc=utf-8 ts=2 sw=0 sts=0 sr et si tw=0 fdm=marker fmr={{{,}}}:
 {
-  description = "Flake for the devshell for `battery_notifications.py`";
+  description = "Flake for the devshell of `batnotifsd` (https://github.com/Andy3153/batnotifsd)";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }:
@@ -17,6 +17,11 @@
         [
           (python3.withPackages(python-pkgs: with python-pkgs;
           [
+            # for creating package
+            build
+            twine
+
+            # build deps
             pydbus
             pygobject3
           ]))
